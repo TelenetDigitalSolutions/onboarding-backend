b690a47b88f71b5184dff3009059496d0a765321
Ief Cuynen <ief.cuynen@telenetgroup.be>
2021-02-19 16:39:07 +0100

    Bare spring boot application

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# onboarding-backend
+# Setup instructions
+
+* Java 11
+* Maven 3.5+
+
+# Available maven artifacts
+
+* Spring boot 2.4.3
+* lombok
+*
+
+## Spring boot starters
+* spring-boot-starter-web
+* spring-boot-starter-test
+* spring-boot-starter-validation
+
+* spring-boot-starter-devtools
+* spring-boot-configuration-processor
---
+++ src/main/java/be/telenetgroup/digitalsolutions/onboardingbackend/OnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package be.telenetgroup.digitalsolutions.onboardingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingBackendApplication.class, args);
+	}
+
+}
